2fb7bbc81d338a4d16ed1f0eefd24b2836c60fed
WinnessRakotozafy <winness.rakotozafy@outlook.fr>
2022-09-14 21:08:04 +0200

    # Function: Add cursor to typing

--- src/main.js
+++ src/main.js
@@ -1,15 +1,13 @@
-/* Sample function test for typing text */
+// Sample function test for typing text 
 let i = 0
-let text = "Hi there! The name's Winness, codename 'WRKT.', a willing Cybersecurity Engineer who wants to be specialized in the field of programming and automating security policies in system and network infrastructure. I am currently studying at CCI Campus Alsace to learn the fundamentals of networking and system security, but also introduction to algorithm and programming from scratch. I am always excited to learn new technologies, especially about automating tasks and security operation. Want to know more about my journey and projects ? Feel free to go here:"
-
-let speed = 40
+const text = "Hi there! The name's Winness, codename 'WRKT.', a willing Cybersecurity Engineer who wants to be specialized in the field of programming and automating security policies in system and network infrastructure. I am currently studying at CCI Campus Alsace to learn the fundamentals of networking and system security, but also introduction to algorithm and programming from scratch. I am always excited to learn new technologies, especially about automating tasks and security operation."
+let speed = 35
 
 function typing(){
-    if (i < text.length) {
+    if (i < text.length){
         document.querySelector(".typing").innerHTML += text.charAt(i);
         i++;
         setTimeout(typing, speed);
     }
 }
-
 typing()

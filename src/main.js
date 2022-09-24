7f454afc5e502e950999e1185c7424e5c05a871b
WinnessRakotozafy <winness.rakotozafy@outlook.fr>
2022-09-24 08:09:50 +0200

    # Design : Add new favico; Change text color

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 // Sample function test for typing text 
 let i = 0
-const text = "Hi there! The name's Winness, codename 'WRKT.', a willing Cybersecurity Engineer who wants to be specialized in the field of programming and automating security policies in system and network infrastructure. I am currently studying at CCI Campus Alsace to learn the fundamentals of networking and system security, but also introduction to algorithm and programming from scratch. I am always excited to learn new technologies, especially about automating tasks and security operation."
-let speed = 35
+let text = "Hi there! The name's Winness, codename 'WRKT.', a willing Cybersecurity Engineer who wants to be specialized in the field of programming and automating security policies in system and network infrastructure. I am currently studying at CCI Campus Alsace to learn the fundamentals of networking and system security, but also introduction to algorithm and programming from scratch. I am always excited to learn new technologies, especially about automating tasks and security operation."
+let speed = 34
 
 function typing(){
     if (i < text.length){
@@ -11,3 +11,5 @@ function typing(){
     }
 }
 typing()
+
+

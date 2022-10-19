096c7414d5cbdf7005202a5860ae5eafd12b0294
WinnessRakotozafy <winness.rakotozafy@outlook.fr>
2022-10-19 18:44:54 +0200

    # Features: Add new HTML pages

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 // Sample function test for typing text 
 let i = 0
-let text = "Hi there! The name's Winness, codename 'WRKT.', a willing Cybersecurity Engineer who wants to be specialized in the field of programming and automating security policies in system and network infrastructure. I am currently studying at CCI Campus Alsace to learn the fundamentals of networking and system security, but also introduction to algorithm and programming from scratch. I am always hyped to learn new technologies, especially about automating tasks and security operation."
+let text = "Hi there! Je me presente: Winness, codename 'WRKT.', jeune technicien système et réseau qui voudrait se spécialiser dans la conception, la programmation et l'application des mesures de sécurité sur un système d'information, en vue de devenir un ingenieur en Cybersécurité capable de faire face aux divers aléas du cyberespace."
 let speed = 34
 
 function typing(){

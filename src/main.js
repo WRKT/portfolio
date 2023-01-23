92911f490dafff262c448d6a75a7a29f9903f3f3
WinnessRakotozafy <winness.rakotozafy@outlook.fr>
2023-01-23 18:16:36 +0100

    # Add arrow --> return top page

---
+++ src/main.js
@@ -0,0 +1,17 @@
+// Navbar responsive function
+function Menu(e){
+    let list = document.querySelector('ul');
+
+    e.name === 'menu-sharp' ? (e.name = "close",list.classList.add('top-[80px]'), list.classList.add('opacity-100')): (e.name = "menu-sharp",list.classList.remove('top-[80px]'),list.classList.remove('top-[80px]'))
+}
+
+// Arrow button
+let button = document.getElementById("scroll-button");
+
+window.onscroll = function() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    button.style.display = "block";
+  } else {
+    button.style.display = "none";
+  }
+}
